16f878c642b59ca4828e4696da3500565155b4e0
Mark <mrviscods@gmail.com>
2019-10-13 21:07:25 -0400

    Add files via upload

---
+++ Data/Project 3 Table Schemata.sql
@@ -0,0 +1,54 @@
+﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- Link to schema: https://app.quickdatabasediagrams.com/#/d/sKlOlU
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+
+
+
+CREATE TABLE "aroma_list" (
+    "tier_1" VARCHAR(50),
+    "tier_2" VARCHAR(50),
+	"tier_3" VARCHAR(50),
+	"alt_name" VARCHAR(50)
+    CONSTRAINT "pk_departments" PRIMARY KEY (
+        "dept_no"
+);
+
+CREATE TABLE "wine_words" (
+	"ID" FLOAT,
+    "wine" VARCHAR(100),
+    "attribute" VARCHAR(50),
+    "score" FLOAT,
+    "price" FLOAT
+
+);
+
+CREATE TABLE "wine_count" (
+    "attribute" VARCHAR(50),
+    "tally" FLOAT
+);
+
+CREATE TABLE "wine_avg" (
+    "attribute" VARCHAR(50),
+    "avg_score" FLOAT,
+    "avg_price" FLOAT
+);
+
+
+ALTER TABLE "dept_emp" ADD CONSTRAINT "fk_dept_emp_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "employees" ("emp_no");
+
+ALTER TABLE "dept_emp" ADD CONSTRAINT "fk_dept_emp_dept_no" FOREIGN KEY("dept_no")
+REFERENCES "departments" ("dept_no");
+
+ALTER TABLE "dept_manager" ADD CONSTRAINT "fk_dept_manager_dept_no" FOREIGN KEY("dept_no")
+REFERENCES "departments" ("dept_no");
+
+ALTER TABLE "dept_manager" ADD CONSTRAINT "fk_dept_manager_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "employees" ("emp_no");
+
+ALTER TABLE "salaries" ADD CONSTRAINT "fk_salaries_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "employees" ("emp_no");
+
+ALTER TABLE "titles" ADD CONSTRAINT "fk_titles_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "employees" ("emp_no");
+
---
+++ Data/Queries.sql
@@ -0,0 +1,75 @@
+--QRY001
+
+--Create view QRY001_emp_details as
+SELECT Employees.emp_no, Employees.last_name, Employees.first_name, Employees.gender, Salaries.salary
+FROM Employees 
+LEFT JOIN Salaries ON Employees.emp_no = Salaries.emp_no;
+
+--QRY002
+
+--Create view QRY002_hired_1986 as
+SELECT Employees.emp_no, Employees.last_name, Employees.first_name, Employees.hire_date
+FROM Employees
+WHERE Employees.hire_date Like '1986%';
+
+--QRY003
+
+--Create view QRY003_dept_mgr_list as
+SELECT Departments.dept_no, Departments.dept_name, Dept_manager.emp_no, Employees.last_name, Employees.first_name, Employees.hire_date, Dept_manager.from_date, Dept_manager.to_date
+FROM Departments 
+INNER JOIN Dept_manager ON Departments.dept_no = Dept_manager.dept_no 
+INNER JOIN Employees ON Dept_manager.emp_no = Employees.emp_no
+WHERE Dept_manager.to_date not Like '9999%'
+
+--QRY004
+
+--create view QRY004_Emp_by_dept as
+SELECT Employees.emp_no, Employees.last_name, Employees.first_name, Departments.dept_name
+FROM Employees
+LEFT JOIN Dept_emp ON Employees.emp_no = Dept_emp.emp_no
+LEFT JOIN Departments ON Dept_emp.dept_no = Departments.dept_no;
+
+--QRY005
+
+--create view QRY005 - emp_Hercules_B as
+SELECT Employees.first_name, Employees.last_name
+FROM Employees
+WHERE Employees.first_name = 'Hercules' 
+and Employees.last_name like 'B%';
+
+--QRY006
+
+--create view QRY006_emp_sales_dept
+SELECT Employees.emp_no, Employees.last_name, Employees.first_name, Departments.dept_name
+FROM Employees 
+INNER JOIN Dept_emp ON Employees.emp_no = Dept_emp.emp_no
+INNER JOIN Departments ON Dept_emp.dept_no = Departments.dept_no
+WHERE Dept_emp.dept_no = 'd007';
+
+--QRY007
+
+--create  view QRY007_emp_sales_dev
+SELECT Employees.emp_no, Employees.last_name, Employees.first_name, Departments.dept_name
+FROM Employees 
+INNER JOIN Dept_emp ON Employees.emp_no = Dept_emp.emp_no
+INNER JOIN Departments ON Dept_emp.dept_no = Departments.dept_no
+WHERE Dept_emp.dept_no = 'd005'
+or Dept_emp.dept_no = 'd007'
+ORDER BY Dept_emp.dept_no;
+
+--QRY008
+
+--create  view QRY008_lastname_count_desc
+SELECT Employees.last_name, Count(Employees.first_name) AS CountOffirst_name
+FROM Employees
+GROUP BY Employees.last_name
+ORDER BY Count(Employees.first_name) DESC;
+
+--QRY009 – Bonus
+
+--create  view QRY009_bonus
+SELECT Titles.title, ROUND(AVG(Salaries.salary)) AS "AVG Salary"
+FROM Titles
+INNER JOIN Salaries ON Titles.emp_no = Salaries.emp_no
+GROUP BY Titles.title
+ORDER BY "AVG Salary" DESC;
